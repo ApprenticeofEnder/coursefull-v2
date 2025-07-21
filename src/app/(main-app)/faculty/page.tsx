9e47cf9e0f0646dc14a9d8b1394c792cd91904fb
Robert Babaev <github@robertbabaev.tech>
2025-07-21 05:01:34 -0400

    feat: add rudimentary client routing structure

---
+++ src/app/(main-app)/faculty/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+import { auth } from "~/server/auth";
+import { HydrateClient, api } from "~/trpc/server";
+
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <HydrateClient>
+      <div>Welcome to CourseFull!</div>
+    </HydrateClient>
+  );
+}
