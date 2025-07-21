38791c8e7f7bcd50cb44fdaabd2a148765c3a464
Robert Babaev <github@robertbabaev.tech>
2025-07-20 20:34:14 -0400

    fix: production worker now actually is up and running (logs don't work tho)

--- scripts/prod-start.ts
+++ scripts/prod-start.ts
@@ -1,15 +1,12 @@
+import { execFile } from "node:child_process";
 import cluster from "node:cluster";
-import { cpus } from "node:os";
+import { availableParallelism } from "node:os";
 import process from "node:process";
 
-const numCPUs = cpus().length;
+const numCPUs = availableParallelism();
 
-function startCluster() {
-  cluster.setupPrimary({
-    exec: require.resolve(".bin/next"),
-    args: ["start", ...process.argv.slice(2)],
-  });
-
+if (cluster.isPrimary) {
+  console.log(`Cluster ${process.pid} started`);
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
@@ -17,8 +14,19 @@ function startCluster() {
   cluster.on("exit", (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`, { code, signal });
   });
-}
+} else {
+  console.log(`Worker ${process.pid} started (id ${cluster.worker?.id})`);
+  const port = 3000 + (cluster.worker?.id ?? 0);
 
-if (cluster.isPrimary) {
-  startCluster();
+  execFile(
+    "node_modules/.bin/next",
+    ["start", "-p", `${port}`, ...process.argv.slice(2)],
+    (error, stdout, stderr) => {
+      if (error) {
+        throw error;
+      }
+      console.log(stdout);
+      console.log(stderr);
+    },
+  );
 }
