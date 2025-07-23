ebacf811f169a2fce7233d4dff2906b604756487
Robert Babaev <github@robertbabaev.tech>
2025-07-23 18:54:06 -0400

    fix: attempt to upgrade node version

--- scripts/db-populate.ts
+++ scripts/db-populate.ts
@@ -1,10 +1,46 @@
+import { readFile } from "node:fs/promises";
+
 import { env } from "~/env";
 import { getLogger } from "~/lib/logger";
 import { db } from "~/server/db";
 import { type NewSchool, schools } from "~/server/db/schema";
 
+interface RawSchool {
+  name: string;
+  domains: string[];
+  web_domains: string[];
+  country: string;
+  alpha_two_code: string;
+  "state-province": string | null;
+}
+
 async function loadUniversities() {
   const logger = getLogger();
   try {
-  } catch (error) {}
+    const schoolDataRaw = await readFile(env.UNIVERSITY_DATA_FILE, "utf8");
+    const schoolData: NewSchool[] = JSON.parse(schoolDataRaw).map(
+      (rawSchool: RawSchool) => {
+        return {
+          name: rawSchool.name,
+          domains: rawSchool.domains,
+          webPages: rawSchool.web_domains,
+          country: rawSchool.country,
+          stateOrProvince: rawSchool["state-province"],
+          alphaTwoCode: rawSchool.alpha_two_code,
+        } as NewSchool;
+      },
+    );
+
+    const insertResult = await db
+      .insert(schools)
+      .values(schoolData)
+      .onConflictDoNothing()
+      .returning();
+
+    logger
+      .withMetadata({ records: insertResult.length })
+      .info("Inserted data successfully.");
+  } catch (error) {
+    logger.withError(error).error("Unexpected error occurred.");
+  }
 }
